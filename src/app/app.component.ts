1e0509062806ab3efee23765c7a0958a8009790c
unknown <pioterofficjal@gmail.com>
2020-09-05 15:24:31 +0200

    AIO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  public todos: string [] = [];
+  public todosForm = new FormGroup({
+    todo: new FormControl('')
+    });
+
+   public onSubmit(): void {
+   this.todos.push(this.todosForm.value.todo);
+    }
 }
+
